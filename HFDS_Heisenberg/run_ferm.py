2fd392b7613035b979bfa27d6295e11783819947
FrancescoConoscenti <fconoscenti@student.ethz.ch>
2025-09-18 16:25:50 +0200

    u

--- HFDS_Heisenberg/run_ferm.py
+++ HFDS_Heisenberg/run_ferm.py
@@ -24,115 +24,113 @@ from netket import experimental as nkx
 import os
 import flax
 os.environ['JAX_TRACEBACK_FILTERING'] = 'off'
+import numpy as np
+
+from netket.operator.spin import sigmax, sigmaz, sigmay
+from netket.experimental.driver import VMC_SRt
 
 from HFDS_model_ferm import Orbitals
 from HFDS_model_ferm import HiddenFermion
 
-from netket.experimental.driver import VMC_SR
 
 
-# parse arguments
+#Physical param
 L = 4
 n_elecs = L*L
+N_sites = L*L
+N_up    = (n_elecs+1)//2
+N_dn    = n_elecs//2
+n_dim = 2
 J2 = 0
+
 dtype   = "real"
 MFinitialization = "Fermi"
 determinant_type = "hidden"
 bounds  ="PBC"
-print("params: J2=", J2, "L=", L)
-
-N_sites = L * L
-N_up    = (n_elecs+1)//2
-N_dn    = n_elecs//2
-
 double_occupancy = False
+save = True
 
-# network parameters and sampling
-lr               = 0.02
-n_samples        = 4096*2
+#Network param
+n_hid_ferm       = 16
+features         = 8
+hid_layers       = 2
+
+n_samples        = 2048
+lr               = 0.0075
+N_opt            = 100
+
 n_chains         = n_samples//2
-n_steps          = 1000
+n_steps          = 10
 n_modes          = 2*L*L
 cs               = n_samples
+n_dim           = 2
 
-#FFN and hidden
-n_hid            = 3
-features         = 3
-layers           = 3
+model_name = f"layers{hid_layers}_hidd{n_hid_ferm}_feat{features}_sample{n_samples}_lr{lr}_iter{N_opt}"
+lattice_name = f"J={J2}_L={L}"
+folder = f'HFDS_Heisenberg/plot/ferm/{model_name}/{lattice_name}'
+os.makedirs(folder, exist_ok=True)  #create folder for the plots and the output file
+sys.stdout = open(f"{folder}/output.txt", "w") #redirect print output to a file inside the folder
 
 
-
-# --------------- define the network  -------------------
+# Lattice and Hilbert space
 boundary_conditions = 'pbc' 
-filename = f"results_sym/energy_{L}x{L}_{boundary_conditions}x{boundary_conditions}_Nup={N_up}_Ndn={N_dn}_lr={lr}_nlayers={layers}_nfeatures={features}_nhid={n_hid}_nsamples={n_samples}_{determinant_type}_"+MFinitialization+"_"+dtype
 g = nk.graph.Hypercube(length=L, n_dim=2, pbc=True, max_neighbor_order=2)
 
-hi = nk.hilbert.SpinOrbitalFermions(N_sites, s = 1/2, n_fermions_per_spin = (N_up, N_dn))
+hi = nkx.hi.SpinOrbitalFermions(N_sites, s = 1/2, n_fermions_per_spin = (N_up, N_dn))
 print(hi.size)
 
 
 if dtype=="real": dtype_ = jnp.float64
 else: dtype_ = jnp.complex128
 
+# Model
 ma = HiddenFermion(n_elecs=n_elecs,
                    network="FFNN",
-                   n_hid=n_hid,
+                   n_hid=n_hid_ferm,
                    L=L,
-                   layers=layers,
+                   layers=hid_layers,
                    features=features,
                    MFinit=MFinitialization,
-                   hilbert=hi,
+                   hi=hi,
                    stop_grad_mf=False,
                    stop_grad_lower_block=False,
                    bounds=bounds,
                    dtype=dtype_)
 
 
-# ------------- define Hamiltonian ------------------------
 # Heisenberg J1-J2 spin hamiltonian
 hamiltonian = nk.operator.Heisenberg(
-    hilbert=hi, graph=g, J=[1.0, J2], sign_rule=[False, False]).to_jax_operator()  # No Marshall sign rule
+    hi=hi, graph=g, J=[1.0, J2], sign_rule=[False, False]).to_jax_operator()  # No Marshall sign rule
 
-
-# ---------- define sampler ------------------------
+#sampler
 exchange_g = nk.graph.disjoint_union(g, g)
 print("Exchange graph size:", exchange_g.n_nodes)
 
 sampler = nk.sampler.MetropolisExchange(hi, graph=exchange_g, d_max=1)
 
+#vstate
 vstate = nk.vqs.MCState(sampler, ma, n_samples=n_samples, chunk_size=cs, n_discard_per_chain=32) #defines the variational state object
 total_params = sum(p.size for p in jax.tree_util.tree_leaves(vstate.parameters))
 print(f'Total number of parameters: {total_params}')
 
+#optimization
+optimizer = nk.optimizer.Sgd(learning_rate=lr)
 
-"""# Draw a sample and compute log amplitude to debug
-sample = vstate.samples[0]
-print(sample)
-val = vstate.log_value(sample)
-print("Initial log amplitude:", val)
-
-# calculate observable
-obs = vstate.expect(hamiltonian)
-print("Initial energy:", obs)"""
-
-
-optimizer = nk.optimizer.Sgd(learning_rate=0.001)
-
-vmc = VMC_SR(
+vmc = VMC_SRt(
     hamiltonian=hamiltonian,
     optimizer=optimizer,
-    diag_shift=1e-3,
+    diag_shift=1e-2,
     variational_state=vstate,
-    mode="real",
+    #mode="real",
 ) 
 
 log = nk.logging.RuntimeLog()
 
-N_opt = 5
 vmc.run(n_iter=N_opt, out=log)
 
 
 #%%
+#Energy
 energy_per_site = log.data["Energy"]["Mean"].real / (L * L * 4)
 
 E_vs = energy_per_site[-1]
@@ -142,5 +140,87 @@ plt.plot(energy_per_site)
 
 plt.xlabel("Iterations")
 plt.ylabel("Energy per site")
-  
-#evaluate any observable...
+plt.savefig(f'{folder}/Energy.png')
+plt.show()
+
+#Correlation function
+vstate.n_samples = 1024
+N_tot = g.n_nodes
+
+corr_r = np.zeros((L, L))
+counts = np.zeros((L, L))
+
+for i in range(N_tot):
+    for j in range(N_tot):
+        r = g.positions[i] - g.positions[j]
+        corr_ij = 0.25 * (sigmaz(hi, i) * sigmaz(hi, j) + sigmax(hi, i) * sigmax(hi, j) + sigmay(hi, i) * sigmay(hi, j))
+        exp = vstate.expect(corr_ij)
+        r0, r1 = int(r[0]) % L , int(r[1]) % L #PBC
+        corr_r[r0, r1] += exp.mean.real
+        counts[r0, r1] += 1
+corr_r /= counts 
+corr_r[0, 0] = 0  # set C(0) = 0
+
+plt.figure(figsize=(6,5))
+plt.imshow(corr_r, origin='lower', cmap='viridis')
+plt.colorbar(label='C(r)')
+plt.xlabel('dx')
+plt.ylabel('dy')
+plt.title('Spin-Spin Correlation Function C(r) in 2D')
+plt.xticks(np.arange(L))  # integer ticks for x-axis
+plt.yticks(np.arange(L)) 
+if(save):
+    plt.savefig(f'{folder}/Corr.png')
+plt.show()
+
+
+#Structure factor
+# Compute the 2D Fourier transform of corr_r
+S_q = np.fft.fft2(corr_r)
+S_q_periodic = np.zeros((L+1, L+1), dtype=S_q.dtype)
+S_q_periodic[:L, :L] = S_q  
+S_q_periodic[L, :] = S_q_periodic[0, :]    
+S_q_periodic[:, L] = S_q_periodic[:, 0]    
+
+plt.figure(figsize=(6,5))
+plt.imshow(np.abs(S_q_periodic), origin='lower', cmap='viridis')
+plt.colorbar(label='|S(q)|')
+plt.xlabel('q_x')
+plt.ylabel('q_y')
+plt.title('Structure Factor S(q)')
+plt.xticks([0, 1/2*L, L], ['0', 'π', '2π'])
+plt.yticks([0, 1/2*L, L], ['0', 'π', '2π'])
+if(save):
+    plt.savefig(f'{folder}/Struct.png')
+plt.show()
+
+#Exact gs
+E_gs, ket_gs = nk.exact.lanczos_ed(hamiltonian, compute_eigenvectors=True)
+
+print(f"Exact ground state energy = {E_gs[0]:.3f}")
+E_exact = E_gs[0]/(L*L*4)
+print(f"Exact ground state energy per site= {E_exact}")
+
+#Relative Error
+e = np.abs((E_vs - E_exact)/E_exact)
+print(f"Relative error = {e}")
+
+import netket.experimental.observable as obs
+import netket.operator as op
+
+#Total magnetization on Z
+tot_magn = sum([sigmaz(hi, i) for i in g.nodes()])
+tot_magn_vstate = vstate.expect(tot_magn).mean.real
+print(f"Magnetization = {tot_magn_vstate}" )
+
+#Variance
+variance = log.data["Energy"]["Variance"][-1].real
+print(f"Variance = {variance}")
+
+#Vscore
+v_score = L*L*variance/(E_vs*L*L*4)
+print(f"V-score = {v_score}")
+
+
+sys.stdout.close()
+
--- HFDS_Heisenberg/run_spin.py
+++ HFDS_Heisenberg/run_spin.py
@@ -24,52 +24,59 @@ import os
 import flax
 os.environ['JAX_TRACEBACK_FILTERING'] = 'off'
 import matplotlib.pyplot as plt
+import numpy as np
 
+from netket.operator.spin import sigmax, sigmaz, sigmay
 # Variational monte carlo driver
-from netket.experimental.driver import VMC_SR
+from netket.experimental.driver import VMC_SRt
 
 from HFDS_model_spin import Orbitals
 from HFDS_model_spin import HiddenFermion
 #%%
-load = True
 
+#Physical param
 L      = 4
-n_elecs = 16
-J2      = 0
-b1      = 0
-b2      = 0
-dtype   = "real"
-MFinitialization = "Fermi"
-determinant_type = "hidden"
-bounds  = {1:{1:"OBC"}, 0:{0:"PBC"}}[b1][b2]
-
-print("params: J2=", J2, "L=", L, "bounds=", b1, b2)
-
-# more parameters for the physical system
-pbc     = [{0: True, 1:False}[b1],{0: True, 1:False}[b2]]
+n_elecs = 16 # L*L should be half filling
 N_sites = L*L
 N_up    = (n_elecs+1)//2
 N_dn    = n_elecs//2
+n_dim = 2
+J2 = 0
 
-# network parameters and sampling
-lr               = 0.02
-n_samples        = 4096
+dtype   = "real"
+MFinitialization = "Fermi"
+determinant_type = "hidden"
+bounds  = "PBC"
+save = True
+
+
+#Network param
+n_hid_ferm       = 16
+features         = 8
+hid_layers       = 2
+lr               = 0.0075
+n_samples        = 1024
+N_opt            = 10
+
 n_chains         = n_samples//2
 n_steps          = 10
-n_hid            = 1
-features         = 3
-layers           = 1
 n_modes          = 2*L*L
 cs               = n_samples
-n_dim           = 2
+n_dim            = 2
+
+
+model_name = f"layers{hid_layers}_hidd{n_hid_ferm}_feat{features}_sample{n_samples}_lr{lr}_iter{N_opt}"
+lattice_name = f"J={J2}_L={L}"
+folder = f'HFDS_Heisenberg/plot/spin/{model_name}/{lattice_name}'
+os.makedirs(folder, exist_ok=True)  #create folder for the plots and the output file
+sys.stdout = open(f"{folder}/output.txt", "w") #redirect print output to a file inside the folder
 
 
 # --------------- define the network  -------------------
-boundary_conditions = 'pbc' if pbc[0] else 'obc'
-folder = f"results/energy_{L}x{L}_{boundary_conditions}x{boundary_conditions}_Nup={N_up}_Ndn={N_dn}J2={J2}_lr={lr}_nlayers={layers}_nfeatures={features}_nhid={n_hid}_nsamples={n_samples}_{determinant_type}_{MFinitialization}_{dtype}"
+boundary_conditions = 'pbc' 
 lattice = nk.graph.Hypercube(length=L, n_dim=n_dim, pbc=True, max_neighbor_order=2)
-#hi = nk.hilbert.SpinOrbitalFermions(N_sites, s = 1/2, n_fermions_per_spin = (N_up, N_dn))
-hi = nk.hilbert.Spin(s=1 / 2, N=lattice.n_nodes, total_sz=0)
+#hi = nk.hi.SpinOrbitalFermions(N_sites, s = 1/2, n_fermions_per_spin = (N_up, N_dn))
+hi = nk.hi.Spin(s=1 / 2, N=lattice.n_nodes, total_sz=0)
 print(hi.size)
 
 
@@ -78,13 +85,13 @@ else: dtype_ = jnp.complex128
 
 model = HiddenFermion(n_elecs=n_elecs,
                    network="FFNN",
-                   n_hid=n_hid,
+                   n_hid=n_hid_ferm,
                    Lx=L,
                    Ly=L,
-                   layers=layers,
+                   layers=hid_layers,
                    features=features,
                    MFinit=MFinitialization,
-                   hilbert=hi,
+                   hi=hi,
                    stop_grad_mf=False,
                    stop_grad_lower_block=False,
                    bounds=bounds,
@@ -93,15 +100,15 @@ model = HiddenFermion(n_elecs=n_elecs,
 #model = nk.models.RBM(alpha=1)
 
 # ------------- define Hamiltonian ------------------------
-# Heisenberg J1-J2 spin hamiltonian
+# Heisenberg J1-J2 spin ha
 ha = nk.operator.Heisenberg(
-    hilbert=hi, graph=lattice, J=[1.0, J2], sign_rule=[False, False]
+    hi=hi, graph=lattice, J=[1.0, J2], sign_rule=[False, False]
 ).to_jax_operator()  # No Marshall sign rule
 
 
 # ---------- define sampler ------------------------
 sampler = nk.sampler.MetropolisExchange(
-    hilbert=hi,
+    hi=hi,
     graph=lattice,
     d_max=1,
     n_chains=n_samples,
@@ -113,6 +120,7 @@ vstate = nk.vqs.MCState(sampler, model, n_samples=n_samples, chunk_size=cs, n_di
 total_params = sum(p.size for p in jax.tree_util.tree_leaves(vstate.parameters))
 print(f'Total number of parameters: {total_params}')
 
+"""
 # Draw a sample and compute log amplitude to debug
 sample = vstate.samples[0]
 print(sample)
@@ -122,34 +130,116 @@ print("Initial log amplitude:", val)
 # calculate observable
 obs = vstate.expect(ha)
 print("Initial energy:", obs)
+"""
 
 #%%
 
 optimizer = nk.optimizer.Sgd(learning_rate=0.01)
 
-vmc = VMC_SR(
-    hamiltonian=ha,
+vmc = VMC_SRt(
+    ha=ha,
     optimizer=optimizer,
     diag_shift=0.1,
     variational_state=vstate,
-    mode = 'real'
+    #mode = 'real'
 ) 
 
 log = nk.logging.RuntimeLog()
 
-N_opt = 3
 vmc.run(n_iter=N_opt, out=log)
 
 
 #%%
+#Energy
 energy_per_site = log.data["Energy"]["Mean"].real / (L * L * 4)
-
 E_vs = energy_per_site[-1]
 print("Last value: ", energy_per_site[-1])
 
 plt.plot(energy_per_site)
-
 plt.xlabel("Iterations")
 plt.ylabel("Energy per site")
+plt.savefig(f'{folder}/Energy.png')
 plt.show()
+
+
+#Correlation function
+vstate.n_samples = 1024
+N_tot = lattice.n_nodes
+
+corr_r = np.zeros((L, L))
+counts = np.zeros((L, L))
+
+for i in range(N_tot):
+    for j in range(N_tot):
+        r = lattice.positions[i] - lattice.positions[j]
+        corr_ij = 0.25 * (sigmaz(hi, i) * sigmaz(hi, j) + sigmax(hi, i) * sigmax(hi, j) + sigmay(hi, i) * sigmay(hi, j))
+        exp = vstate.expect(corr_ij)
+        r0, r1 = int(r[0]) % L , int(r[1]) % L #PBC
+        corr_r[r0, r1] += exp.mean.real
+        counts[r0, r1] += 1
+corr_r /= counts 
+corr_r[0, 0] = 0  # set C(0) = 0
+
+plt.figure(figsize=(6,5))
+plt.imshow(corr_r, origin='lower', cmap='viridis')
+plt.colorbar(label='C(r)')
+plt.xlabel('dx')
+plt.ylabel('dy')
+plt.title('Spin-Spin Correlation Function C(r) in 2D')
+plt.xticks(np.arange(L))  # integer ticks for x-axis
+plt.yticks(np.arange(L)) 
+if(save):
+    plt.savefig(f'{folder}/Corr.png')
+plt.show()
+
+
+#Structure factor
+# Compute the 2D Fourier transform of corr_r
+S_q = np.fft.fft2(corr_r)
+S_q_periodic = np.zeros((L+1, L+1), dtype=S_q.dtype)
+S_q_periodic[:L, :L] = S_q  
+S_q_periodic[L, :] = S_q_periodic[0, :]    
+S_q_periodic[:, L] = S_q_periodic[:, 0]    
+
+plt.figure(figsize=(6,5))
+plt.imshow(np.abs(S_q_periodic), origin='lower', cmap='viridis')
+plt.colorbar(label='|S(q)|')
+plt.xlabel('q_x')
+plt.ylabel('q_y')
+plt.title('Structure Factor S(q)')
+plt.xticks([0, 1/2*L, L], ['0', 'π', '2π'])
+plt.yticks([0, 1/2*L, L], ['0', 'π', '2π'])
+if(save):
+    plt.savefig(f'{folder}/Struct.png')
+plt.show()
+
+#Exact gs
+E_gs, ket_gs = nk.exact.lanczos_ed(ha, compute_eigenvectors=True)
+
+print(f"Exact ground state energy = {E_gs[0]:.3f}")
+E_exact = E_gs[0]/(L*L*4)
+print(f"Exact ground state energy per site= {E_exact}")
+
+#Relative Error
+e = np.abs((E_vs - E_exact)/E_exact)
+print(f"Relative error = {e}")
+
+import netket.experimental.observable as obs
+import netket.operator as op
+
+#Total magnetization on Z
+tot_magn = sum([sigmaz(hi, i) for i in lattice.nodes()])
+tot_magn_vstate = vstate.expect(tot_magn).mean.real
+print(f"Magnetization = {tot_magn_vstate}" )
+
+#Variance
+variance = log.data["Energy"]["Variance"][-1].real
+print(f"Variance = {variance}")
+
+#Vscore
+v_score = L*L*variance/(E_vs*L*L*4)
+print(f"V-score = {v_score}")
+
+
+sys.stdout.close()
 
